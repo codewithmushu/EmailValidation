2f53b287948cfe00f7267ff5cf1ee296216ccf42
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-01 23:56:56 +0530

    [Muskan] Added : C# program for Email Validation.

---
+++ Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Patterns
+    {
+        public static string REGEX_PINCODE = "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$";
+
+        public bool validatePinCode(string pinCode)
+        {
+            return Regex.IsMatch(pinCode, REGEX_PINCODE);
+        }
+    }
+}
