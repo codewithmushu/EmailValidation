aed6dbc6ea5c91dd3bebfd3fb4c228be113f2188
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-01 23:50:25 +0530

    [Muskan] Added : First file for email validation program.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your Email ID for validation :");
+
+            Patterns patterns = new Patterns();
+
+            string pin = Console.ReadLine();
+
+            bool val = patterns.validatePinCode(pin);
+
+            
+            if (val)
+            {
+                Console.WriteLine("Pattern Matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern not Matched");
+            }
+        }
+    }
+}
